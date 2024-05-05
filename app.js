0174f1608bd019290056c81a42729f8a964d336d
MycoTutorial <168274248+MycoTutorial@users.noreply.github.com>
2024-05-05 09:48:27 +0530

    code

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const PORT = 8181;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+});
+
+app.get('/', (req, res) => {
+    res.json("Hello World");
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected : ', socket.id);
+
+    socket.on('join-room', (mb) => {
+        console.log('user mobile room : ', mb);
+        socket.join(mb);
+        io.to(mb).emit('user-joined', mb);
+    });
+
+    socket.on('offer', (payload) => {
+        io.to(payload.target).emit('offer', payload);
+    });
+
+    socket.on('answer', (payload) => {
+        io.to(payload.target).emit('answer', payload);
+    });
+
+    socket.on('ice-candidate', (payload) => {
+        io.to(payload.target).emit('ice-candidate', payload.candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on port : ${PORT}`);
+});
