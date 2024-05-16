8ecc841b4f9dff2fd2bdd64178f92ec3c605389c
MycoTutorial <168274248+MycoTutorial@users.noreply.github.com>
2024-05-16 18:07:16 +0530

    updated code

--- app.js
+++ app.js
@@ -9,46 +9,38 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: 'https://video-call-bay.vercel.app',
         credentials: true
     }
-});
-
-app.get('/', (req, res) => {
-    res.json("Hello World");
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected : ', socket.id);
 
     socket.on('join-room', (mb) => {
-        console.log('user mobile room : ', mb);
         socket.join(mb);
         io.to(mb).emit('user-joined', mb);
     });
-
-    socket.on('call-user', (payload) => {
-        console.log('call-user payload : ', payload);
-        io.to(payload.from).emit('call-user', payload);
-    });
-
-    socket.on('call-received', (data) => {
-        io.to(data.target).emit('call-received', data);
-    });
-
     socket.on('offer', (payload) => {
-        console.log("offer payload : ", payload);
+        console.log('offer payload : ', payload);
         io.to(payload.target).emit('offer', payload);
     });
 
     socket.on('answer', (payload) => {
-        console.log("answer payload : ", payload);
+        console.log('answer payload : ', payload);
         io.to(payload.target).emit('answer', payload);
     });
 
     socket.on('ice-candidate', (payload) => {
-        console.log("ice-candidate payload : ", payload);
         io.to(payload.target).emit('ice-candidate', payload.candidate);
+    });
+
+    socket.on('call-ended', (mb) => {
+        io.to(mb).emit('call-ended', mb);
+    });
+
+    socket.on('call-rejected', (mb) => {
+        io.to(mb).emit('call-rejected', mb);
     });
 
     socket.on('disconnect', () => {
